89de82b6bb8ae9944ab0ba4057840eb6d86e9ddd
Maxim Matyushin <matjushin.m@gmail.com>
2017-11-29 18:37:04 +0300

    HT4.2 Реализовать фильтрацию статей для ArticleList

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,30 +1,47 @@
 import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import Article from './Article'
+import Accordion from './common/Accordion'
+import {connect} from 'react-redux'
+import {DateUtils} from 'react-day-picker'
 
-export default class ArticleList extends Component {
-    state = {
-        openArticleId: null
-    }
-
+class ArticleList extends Accordion {
     render() {
-        const articleElements = this.props.articles.map((article, index) => <li key = {article.id}>
-            <Article article = {article}
-                     isOpen = {this.state.openArticleId === article.id}
-                     toggleOpen = {this.toggleOpenArticle}
-            />
-        </li>)
+        const {articles, dateRange, filteredArticles} = this.props
+        const filteredArticlesIDs = filteredArticles.map((article) => article.value)
+
+        if (!articles.length) return <h3>No Articles</h3>
+
+        /*жадный фильтр
+        filter(article => (dateRange.from === null) || DateUtils.isDayAfter(new Date(article.date), dateRange.from)).
+        filter(article => (dateRange.to === null) || DateUtils.isDayBefore(new Date(article.date), dateRange.to)).*/
+        const articleElements = articles.
+        filter(article => DateUtils.isDayInRange(new Date(article.date), dateRange)).
+        filter(article => (filteredArticlesIDs.length === 0) || filteredArticlesIDs.includes(article.id)).
+        map((article) => <li key={article.id}>
+                                <Article article={article}
+                                         isOpen={article.id === this.state.openItemId}
+                                         toggleOpen={this.toggleOpenItemMemoized(article.id)}
+                />
+            </li>)
         return (
             <ul>
                 {articleElements}
             </ul>
         )
     }
-/*
-
-    toggleOpenArticleWitoutCurr(openArticleId) {
-        this.setState({ openArticleId })
-    }
-*/
-
-    toggleOpenArticle = openArticleId => this.setState({ openArticleId })
 }
+
+
+ArticleList.defaultProps = {
+    articles: []
+}
+
+ArticleList.propTypes = {
+    articles: PropTypes.array.isRequired
+}
+
+export default connect(state => ({
+    articles: state.articles,
+    dateRange: state.dateRange,
+    filteredArticles: state.articlesFilter}))(ArticleList)
