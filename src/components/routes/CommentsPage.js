fb283b75d36418060df7995fc3e9a36b52c0fa97
makikam <matjushin.m@gmail.com>
2017-12-12 17:38:59 +0300

    ##HT7.1 Реализовать роут для пагинации всех комментов (/comments/...)

---
+++ src/components/routes/CommentsPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import CommentsPageList from '../CommentsPageList'
+
+
+class CommentsPage extends Component {
+
+    render(){
+        return <h1>Comments Page List</h1>
+    }
+
+}
+
+export default CommentsPage
